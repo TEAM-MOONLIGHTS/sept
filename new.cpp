fd78be987bfe7a8a56ecabcc8425261b9999d213
unknown <cwenhui10@gmail.com>
2023-09-29 22:23:57 +0800

    if else statement

--- new.cpp
+++ new.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void thirdFucntion() {
-    cout << "Third Function";
-}
-
-int main() {
-    thirdFucntion();
-    cout<<"Hello world";
+int main() 
+{
+    double amount,total_amount;
+    cout<<"Enter amount of purchase:";
+    cin>>amount;
+    
+    if (amount<100)
+    {
+        total_amount=amount*0.98;
+        cout<<"total_amount:"<<total_amount;
+    }    
+        else if (amount>=100 && amount<500)
+        {
+            total_amount=amount*0.95;
+            cout<<"total_amount:"<<total_amount;
+        }        
+            else if (amount>=500)
+            {
+                total_amount=amount*0.9;
+                cout<<"total_amount:"<<total_amount;
+            }        
+    else
+    {
+        cout<<"error";       
+    }
     return 0;
 }
